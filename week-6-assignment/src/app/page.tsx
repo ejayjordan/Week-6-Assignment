6691b597a8206ed96eca51b0a3e85344649127ca
jay <emma.jordan@bsu.edu>
2024-10-03 19:38:59 -0400

    creation of onClick for button

--- week-6-assignment/src/app/page.tsx
+++ week-6-assignment/src/app/page.tsx
@@ -4,14 +4,18 @@ import Image from "next/image";
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
   
-  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleChange = (event: any) => {
     setInputValue(event.target.value);
   };
+
+  const getInput = (event: any) => {
+    
+  }
 
   return (
     <div>
     <input type="text" value={inputValue} onChange={handleChange}></input>
-    <button></button>
+    <button onClick={getInput}></button>
     </div>
   );
 }
