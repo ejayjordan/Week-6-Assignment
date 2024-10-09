298632f180ff08feff9be614acc6e6b2e4970d1b
jay <emma.jordan@bsu.edu>
2024-10-09 19:20:09 -0400

    trying some stuff

--- week-6-assignment/src/app/page.tsx
+++ week-6-assignment/src/app/page.tsx
@@ -1,21 +1,31 @@
+'use client'
 import React, { useState } from 'react';
-import Image from "next/image";
 
-export default function Home() {
-  const [inputValue, setInputValue] = useState('');
+ function submitRecipies([]){
+  const [inputValue, setInputValue] = useState([]);
   
   const handleChange = (event: any) => {
     setInputValue(event.target.value);
   };
 
-  const getInput = (event: any) => {
-    
+    return [inputValue, handleChange];
   }
+
+  export default function Home() {
+    const [inputValues, handleChange] = submitRecipies(["name"]);
+
+    const getInput = (event: any) => {
+      event.preventDefault();
+      console.log(inputValues)
+    };
 
   return (
     <div>
-    <input type="text" value={inputValue} onChange={handleChange}></input>
-    <button onClick={getInput}></button>
+      <form onSubmit={getInput}>
+    <input type="text" name="name" value={inputValues} onChange={handleChange}></input>
+    <button type="submit">Submit</button>
+    <p>You entered: {inputValues}</p>    
+      </form>
     </div>
   );
 }
--- week-6-assignment/src/app/todos.js
+++ week-6-assignment/src/app/todos.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
 import rootReducer from './reducer'
+import [ inputValues, handleChange ] from './page'
 
 const toDos = createToDos(rootReducer)
-
-export default toDos
+ 
+export default async function Array() {
+  let allPosts = await handleChange.select().from(inputValues)
+  return (
+    <ul>
+      {allPosts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
